f186607e3f7df3bdf163752f159d80f1d0144da6
Гилфанова Дарья Ренатовна <drgilfanova@edu.hse.ru>
2025-05-07 22:00:38 +0300

    Добавила базовый код в файл, который читает токен из .env файла

---
+++ bot/config.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN не найден в файле .env. Ошибка!") #чтобы отследить ошибку, и видеть откуда она возникла
