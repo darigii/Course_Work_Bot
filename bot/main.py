0f08933c9ffad4f8c0d2c6cc5af9962bed2079e5
Гилфанова Дарья Ренатовна <drgilfanova@edu.hse.ru>
2025-05-08 17:46:58 +0300

    Было измненено инициализирование бота, сделала через DefaultBotProperties, так как использую последнюю версию aiogram

--- bot/main.py
+++ bot/main.py
@@ -2,16 +2,17 @@ import asyncio
 import logging 
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 from bot.config import BOT_TOKEN
 #логирование, которые будет предупреждать об ошибках и выводить все данные о этой ошибки.
-logging.basicConfig()(
+logging.basicConfig(
     level = logging.INFO, 
     format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__) 
 logger.info("Бот запущен!")
-bot = Bot(token=BOT_TOKEN, parse_mode = ParseMode.HTML)
+bot = Bot(token=BOT_TOKEN,default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(memory = MemoryStorage)
 register_handlers(dp)
 
