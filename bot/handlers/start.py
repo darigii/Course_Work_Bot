72baf583bc505c18d79079e289ce3d65a177b013
Гилфанова Дарья Ренатовна <drgilfanova@edu.hse.ru>
2025-05-09 21:53:31 +0300

    Обновила код, сделала чтобы кнопки были ввиде меню

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,23 +1,31 @@
-from aiogram import Router
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from aiogram import Router, F
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from aiogram.filters import Command
 from database.models import get_user_by_telegram_id
 start_router = Router()
+
 @start_router.message(Command("start"))
 async def start_handler(message: Message):
+    await message.answer(
+        "Добро пожаловать в KoreaBox! \n\n"
+        "Мы — онлайн-магазин корейских товаров: косметика, еда, альбомы и многое другое.\n"
+        "Работаем с любовью к каждому клиенту и доставляем по всей России. 🇰🇷📦\n\n"
+        "Если вы впервые в нашем магазине, то при регистрации можно получить скидку 25% на первый заказ!\n\n"
+        "Чтобы перейти к функциям, используйте команду /menu.")
+    
+@start_router.message(Command("menu"))
+async def show_menu(message: Message):
     user = get_user_by_telegram_id(message.from_user.id)
     if user:
-        markup = ReplyKeyboardMarkup(
-            keyboard=[[KeyboardButton(text="Перейти к покупкам 🛍️", web_app=WebAppInfo(url="https://lambent-tartufo-95748f.netlify.app/"))]],resize_keyboard=True)
-        await message.answer("С возвращением! Готовы выбрать товар?", reply_markup=markup)
+        menu = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="✅ Зарегистрироваться", callback_data="register")],
+            [InlineKeyboardButton(text="🛍 Перейти к покупкам", web_app=WebAppInfo(url="https://lambent-tartufo-95748f.netlify.app/"))],
+            [InlineKeyboardButton(text="Админ-панель", callback_data="admin")]
+        ])
     else:
-        markup = ReplyKeyboardMarkup(
-            keyboard=[
-                [KeyboardButton(text="Зарегистрироваться")],
-                [KeyboardButton(text="Нет, спасибо, перейти к покупкам",web_app=WebAppInfo(url="https://lambent-tartufo-95748f.netlify.app/"))]],resize_keyboard=True)
-        await message.answer(
-            "Добро пожаловать в KoreaBox! 🎉\n"
-            "Хотите зарегистрироваться и получить скидку 25% на первый заказ?",
-            reply_markup=markup
-        )
-
+        menu = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="✅ Зарегистрироваться", callback_data="register")],
+            [InlineKeyboardButton(text=" Отказаться от регистрации", callback_data="skip_registration")],
+            [InlineKeyboardButton(text="Админ-панель", callback_data="admin")]
+        ])
+    await message.answer("📋 Главное меню:", reply_markup=menu)
