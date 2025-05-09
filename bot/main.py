5ff0268eb02d63396d850f9f30c75a9987dcfbc7
Гилфанова Дарья Ренатовна <drgilfanova@edu.hse.ru>
2025-05-09 17:01:44 +0300

    Обновлен код для запуска бота

--- bot/main.py
+++ bot/main.py
@@ -5,6 +5,9 @@ from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 from bot.config import BOT_TOKEN
+from bot.handlers import register_handlers
+from database.models import init_db
+init_db()
 #логирование, которые будет предупреждать об ошибках и выводить все данные о этой ошибки.
 logging.basicConfig(
     level = logging.INFO, 
@@ -13,11 +16,11 @@ logging.basicConfig(
 logger = logging.getLogger(__name__) 
 logger.info("Бот запущен!")
 bot = Bot(token=BOT_TOKEN,default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher(memory = MemoryStorage)
+dp = Dispatcher(storage=MemoryStorage())
 register_handlers(dp)
 
 async def main():
     await dp.start_polling(bot)
-if __name__ == "__name__":
+if __name__ == "__main__":
     asyncio.run(main())
 
