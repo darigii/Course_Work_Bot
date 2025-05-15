f78c645f7dff0c24131da062f07616cdee1605c3
Гилфанова Дарья Ренатовна <drgilfanova@edu.hse.ru>
2025-05-15 16:42:13 +0300

    Добавлена база с админами

--- bot/config.py
+++ bot/config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN не найден в файле .env. Ошибка!") #чтобы отследить ошибку, и видеть откуда она возникла
+    raise ValueError("BOT_TOKEN не найден в файле .env. Ошибка!") #чтобы отследить ошибку
 ADMINS = [1346752839]
