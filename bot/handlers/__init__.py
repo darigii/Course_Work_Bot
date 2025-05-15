a83a7ed2f6a5f17ffa59836a21d20873919c882e
Гилфанова Дарья Ренатовна <drgilfanova@edu.hse.ru>
2025-05-15 22:23:56 +0300

    add

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,7 +1,10 @@
 from .start import start_router
 from .registration import registration_router
 from .admin import admin_router 
+from .start import support_router
 def register_handlers(dp):
     dp.include_router(start_router)
     dp.include_router(registration_router)
     dp.include_router(admin_router)
+    dp.include_router(support_router)
+    
